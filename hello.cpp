62fcc066d8f59e8abf2ecad6b5cabca7e7cdfc43
devjeetmandal09 <devjeetmandal0915@gmail.com>
2017-10-26 15:08:14 +0530

    Hello World

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*
+ *  Author: Devjeet Mandal
+ *
+ */
+// C++ program to print Hello World
+
+
+#include <iostream>    //input output stream library
+using namespace std;   //std(standard) namespace contains input ouput functions
+
+// main
+int main(void)
+{
+	cout<<"Hello World!!";   //console output(cout) to print Hello World
+}
